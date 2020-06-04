c7b7e2da4ec69e604926836fa77da82ddfbc283c
Raffael Kusolitsch <8142@htl.rennweg.at>
2020-06-04 09:55:59 +0200

    Spielbar

--- html/javascript/main.js
+++ html/javascript/main.js
@@ -13,6 +13,8 @@ const gridHeight = 25;
 let imagePlayer;
 const speed = 25;
 let direction = undefined;
+let appleExists = false;
+let apple;
 
 let body = [];
 let lastX;
@@ -20,105 +22,147 @@ let lastY;
 
 
 function init() {
+    body = [];
     xPlayer = 100;
     yPlayer = 100;
+    direction = undefined;
+    appleExists = false;
 
-    // Image initialization
     imagePlayer = new Image();
-    imagePlayer.onload = function () {
-        ctx.drawImage(imagePlayer, xPlayer, yPlayer);
-    }
-    imagePlayer.src = 'images/head.png';
-
+    imagePlayer.src = 'images/body.png';
+    let child = {'x': xPlayer, 'y': yPlayer, 'image': imagePlayer};
+    body.push(child);
 }
 
-window.addEventListener("keydown", changeDirection)
+window.addEventListener('keydown', changeDirection);
 
 requestAnimationFrame(draw);
-window.setInterval(draw, 250);
+window.setInterval(draw, 50);
 
 function changeDirection(event) {
     // down
-    if (event.key === "ArrowDown") {
+    if (event.key === 'ArrowDown') {
         direction = 'down';
     }
     // up
-    if (event.key === "ArrowUp") {
+    if (event.key === 'ArrowUp') {
         direction = 'up';
     }
     // left
-    if (event.key === "ArrowLeft") {
+    if (event.key === 'ArrowLeft') {
         direction = 'left'
     }
 
     // right
-    if (event.key === "ArrowRight") {
+    if (event.key === 'ArrowRight') {
         direction = 'right';
     }
 
-    if (event.key === " ") {
-        addChildren();
-    }
 }
 
 function draw() {
     ctx.clearRect(0, 0, width, height);
     drawSnake();
-    ctx.drawImage(imagePlayer, xPlayer, yPlayer);
+    drawApple();
+    checkApple();
+    checkCollision();
 }
 
 
 function drawSnake() {
-    if (direction === "down") {
+    if (direction === 'down') {
         if (yPlayer < 500-gridHeight) {
-            yPlayer += speed;
+            if (!body.filter(e => body.filter(k => k !== e && e.x === k.x && e.y === yPlayer + speed || console.log("k:", k," e:", e)))) {
+
+                yPlayer += speed;
+            }
         } else {
-            yPlayer = 500 - gridHeight;
+            lost();
         }
     }
     // up
-    if (direction === "up") {
-        if (yPlayer - speed > 0) {
+    if (direction === 'up') {
+        if (yPlayer - speed >= 0) {
             yPlayer -= speed;
         } else {
-            yPlayer = 0;
+            lost();
         }
     }
     // left
     if (direction === 'left') {
-        if (xPlayer - speed > 0) {
+        if (xPlayer - speed >= 0) {
             xPlayer -= speed;
         } else {
-            xPlayer = 0;
+            lost();
         }
     }
 
     // right
     if (direction === 'right') {
-        if (xPlayer + gridWidth + speed < width) {
+        if (xPlayer + gridWidth + speed <= width) {
             xPlayer += speed;
         } else {
-            xPlayer = width-gridWidth;
+            lost();
         }
     }
 
-    body.forEach(drawBody);
+    moveBody();
+    drawBody();
 }
 
-function addChildren() {
+function addChild() {
     let imageChild = new Image();
     imageChild.src = 'images/body.png';
-    let child = {'x': lastX, 'y': lastY, 'image': imageChild};
+    let child = {'x': lastX, 'y': lastY, 'image': imageChild, };
     body.push(child);
 }
 
-function drawBody(entry) {
-    ctx.drawImage(entry.image, entry.x, entry.y);
-    let child = body.pop();
-    lastX = child.x;
-    lastY = child.y;
-    child.x = xPlayer;
-    child.y = yPlayer;
-    body.unshift(child);
+function drawBody() {
+    body.forEach(function(entry) {
+        ctx.drawImage(entry.image, entry.x, entry.y);
+    });
 }
 
+function moveBody() {
+    let child = body.shift();
+    child.x = xPlayer;
+    child.y = yPlayer;
+    body.push(child);
+}
+
+function drawApple() {
+    if (!appleExists) {
+        let x;
+        let y;
+        do {
+            x = Math.floor(Math.random()*width/25)*25;
+            y = Math.floor((Math.random()*height)/25)*25;
+        } while(body.some(e => e.x === x) && body.some(e => e.y === y));
+
+        let image = new Image();
+        image.src = 'images/pcoin.png';
+        apple = {'x': x, 'y': y, 'image': image};
+        appleExists = true;
+    }
+
+    ctx.drawImage(apple.image, apple.x, apple.y);
+}
+
+function checkApple() {
+    if(body[body.length-1].x === apple.x && body[body.length-1].y === apple.y) {
+        appleExists = false;
+        addChild();
+    }
+}
+
+function checkCollision() {
+    if (body.filter(e => body.filter(k => e.x === k.x && e.y === k.y).length >= 2).length >= 2) {
+        lost();
+    }
+
+}
+
+function lost() {
+    console.log('lost');
+    init()
+}
